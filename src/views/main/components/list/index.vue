d419149fc693a18cbc75e332953213e9f2e08826
muzi <xiumuds@163.com>
2025-01-18 22:42:41 +0800

    infinite结合waterfull实现长列表瀑布流

--- src/views/main/components/list/index.vue
+++ src/views/main/components/list/index.vue
@@ -10,30 +10,57 @@ import { isMobileTerminal } from '@/utils/flexible'
  */
 let query = {
   page: 1,
-  size: 20
+  size: 5
 }
+// 数据是否在加载中
+const loading = ref(false)
+// 是否全部加载完成
+const isFinished = ref(false)
+// 数据源
 const pexelsList = ref([])
-const getPexelsData = async (params) => {
-  const res = await getPexelsList(params)
-  pexelsList.value = res.list
+/**
+ * 加载数据的方法
+ */
+const getPexelsData = async () => {
+  if (isFinished.value) return
+  // 完成第一次请求之后，后续的请求让page自增
+  if (pexelsList.value.length) {
+    query.page++
+  }
+  const res = await getPexelsList(query)
+  if (query.page === 1) {
+    pexelsList.value = res.list
+  } else {
+    pexelsList.value.push(...res.list)
+  }
+  // 判断是否全部加载完成
+  if (pexelsList.value.length === res.total) {
+    isFinished.value = true
+  }
+  // 修改 loading 状态
+  loading.value = false
 }
-
-getPexelsData(query)
 </script>
 
 <template>
   <div>
-    <m-waterfall
-      class="px-1 w-full"
-      :data="pexelsList"
-      :nodeKey="'id'"
-      :column="isMobileTerminal ? 2 : 5"
-      :picturePreReading="false"
+    <m-infinite
+      v-model="loading"
+      :isFinished="isFinished"
+      @onLoad="getPexelsData"
     >
-      <template v-slot="{ item, width }">
-        <item-vue :data="item" :width="width"></item-vue>
-      </template>
-    </m-waterfall>
+      <m-waterfall
+        class="px-1 w-full"
+        :data="pexelsList"
+        :nodeKey="'id'"
+        :column="isMobileTerminal ? 2 : 5"
+        :picturePreReading="false"
+      >
+        <template v-slot="{ item, width }">
+          <item-vue :data="item" :width="width"></item-vue>
+        </template>
+      </m-waterfall>
+    </m-infinite>
   </div>
 </template>
 
